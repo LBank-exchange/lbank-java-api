e6270cdbff2bfe0e8ec83d9081f7b1a334af344b
LBank-exchange <39615808+LBank-exchange@users.noreply.github.com>
2019-11-01 16:10:30 +0800

    Update Contant.java

--- src/main/java/com/lbank/java/api/sdk/entity/Contant.java
+++ src/main/java/com/lbank/java/api/sdk/entity/Contant.java
@@ -5,5 +5,5 @@ package com.lbank.java.api.sdk.entity;
  */
 public class Contant {
     //根路径
-    public static final String BASE_URL = "http://api.lbank.info/";
+    public static final String BASE_URL = "https://www.lbkex.net/";
 }
